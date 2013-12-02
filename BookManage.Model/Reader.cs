cc92c08eb49550a4bd45f81bb5c733ea09e14d3d
raojun <534645365@qq.com>
2013-12-02 18:03:24 +0800

    library manage

---
+++ BookManage.Model/Reader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookManage.Model
+{
+    public class Reader
+    {
+        [Serializable]
+
+        public Reader() { }
+        #region 公有属性
+        public int rdID { get; set; }
+        public string rdName { get; set; }
+        public string rdSex { get; set; }
+        public int rdType { get; set; }
+        public string rdDept { get; set; }
+        public string rdPhone { get; set; }
+        public string rdEmail { get; set; }
+        public DateTime rdDateReg { get; set; }
+        public Byte[] rdPhoto { get; set; }
+        public string rdStatus { get; set; }
+        public int rdBorrowQty { get; set; }
+        public string rdPwd { get; set; }
+        public int rdAdminRoles { get; set; }
+        #endregion
+    }
+}
---
+++ BookManage.Model/ReaderType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookManage.Model
+{
+    ///<summary>
+    ///实体类ReaderType
+    ///</summary>
+    [Serializable]
+
+    public class ReaderType
+    {
+        public ReaderType() { }
+        //C# code in VS 2008 or 2010,can do like this.
+        #region 公有属性
+        public int rdType { get; set; }
+        public string rdTypeName { get; set; }
+        public int CanLendQty { get; set; }
+        public int CanLendDay { get; set; }
+        public int CanContinueTimes { get; set; }
+        public float PunishRate { get; set; }
+        public int DateValid { get; set; }
+        #endregion
+
+        //添加复制函数
+        public ReaderType(ReaderType rt)
+        {
+            this.rdType = rt.rdType;
+            this.rdTypeName = rt.rdTypeName;
+            this.CanLendQty = rt.CanLendQty;
+            this.CanLendDay = rt.CanLendDay;
+            this.CanContinueTimes = rt.CanContinueTimes;
+            this.PunishRate = rt.PunishRate;
+            this.DateValid = rt.DateValid;
+        }
+    }
+}
