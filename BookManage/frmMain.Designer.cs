58ac4563317bb4da43f9581b95c2e20b0a2925f1
raojun <534645365@qq.com>
2013-12-08 11:02:30 +0800

    clean

--- BookManage/frmMain.Designer.cs
+++ BookManage/frmMain.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "frmMain";
             this.Text = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- BookManage/frmMain.cs
+++ BookManage/frmMain.cs
@@ -15,5 +15,7 @@ namespace BookManage
         {
             InitializeComponent();
         }
+    
+        public 
     }
 }
